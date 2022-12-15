e9802a45594f8de9260bc7cc891bde6d8966d480
Stefano.In91 <ste.ince91@gmail.com>
2022-12-15 16:52:11 +0100

    milestone 1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   $letters = ["a","b","c","d","e","f","g","h","i","j"];
   $capital_letters = ["A","B","C","D","E","F","G","H","I","J"];
-  $special_characters = ["|","!","£","$","%","/","(",")","=","?"];
+  $special_characters = ["|","!","£","$","&","%","/","(",")","=","?"];
   $numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
   $psw_characters = [
@@ -14,17 +14,16 @@
   ];
 
   if( !empty($psw_length) ) {
-    $new_pw = [];
+    $new_pw = "";
     for ($i = 0; $i < $psw_length; $i++ ) {
       // randomizzazione tipo di carattere
       $random_type = $psw_characters[rand(0, count($psw_characters)-1)];
       // randomizzazione carattere in tipo selezionato
       $random_character = $random_type[rand(0, count($random_type)-1)];
-      $new_pw[] = $random_character;
+      $new_pw .= $random_character;
     };
-
-    var_dump(implode($new_pw));
   };
+  
 ?>
 
 
@@ -42,5 +41,8 @@
       <input  id="psw_length" name="psw_length" type="number" min="3" max="20" placeholder="3 >= num <= 20">
       <button>Invia</button>
     </form>
+    <?php if( !empty($psw_length)) {?>
+      <h1> <?php echo $new_pw ?> </h1>
+    <?php }?>
   </body>
 </html>
